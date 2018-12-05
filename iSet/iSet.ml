70051fe011b88d51df3f6df2740e3dd5c0af2ab0
Michal Siennicki <sienmich@gmail.com>
2018-12-05 09:27:46 +0100

    Poprawione komentarze, literówki, małe błędy

--- iSet/iSet.ml
+++ iSet/iSet.ml
@@ -1,6 +1,11 @@
 (*
+ * Author: Michał Siennicki
+ * Reviewer: Jakub "MrQubo" Nowak *)
+
+(*
  * ISet - Interval sets
- * Copyright (C) 1996-2003 Xavier Leroy, Nicolas Cannasse, Markus Mottl, Jacek Chrzaszcz, Michał Siennicki
+ * Copyright (C) 1996-2003 Xavier Leroy, Nicolas Cannasse, Markus Mottl,
+ * Jacek Chrzaszcz, Michał Siennicki
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -15,26 +20,23 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *)
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *)
 
 (* Interval Set.
  *
  *  This is an interval set, i.e. a set of integers, where large
  *  intervals can be stored as single elements. Intervals stored in the
- *  set are disjoint. 
- *)
+ *  set are disjoint. *)
  
-(**Typ zbioru przedziałów oparty na drzewach AVL,
- * w których różnica wysyokości poddrzew jest zawsze <= 2
+(* Typ zbioru przedziałów oparty na drzewach AVL,
+ * w których różnica wysokości poddrzew jest zawsze <= 2
  * Wszystkie przedziały są zawsze rozłączne,
- * zawsze przedział [x, y] spełnia x<=y, 
+ * zawsze przedział [x, y] spełnia x<=y,
  * nie ma nigdy jednocześnie przedziałów [x, y] i [y+1, z].
  * Kolejne wartości to lewe poddrzewo, przedział,
- * prawe poddrzewo, wysykość drzewa, ilość liczb całkowitych,
- * które są w tym drzewie (do funkcji below)
- *)
-type t =
+ * prawe poddrzewo, wysokość drzewa, liczba liczb całkowitych,
+ * które są w tym drzewie. *)
+ type t =
   | Empty
   | Node of t * (int * int) * t * int * int
 
@@ -45,13 +47,13 @@ let height = function
   | Node (_, _, _, h, _) -> h
   | Empty -> 0
 
-(* Zwraca ilość liczb całkowitych,
- * które są w tym drzewie (do funkcji below)*)
-let ile_int= function
+(* Zwraca liczbę liczb całkowitych,
+ * które są w tym drzewie *)
+let count_ints = function
   | Node (_, _, _, _, h) -> h
   | Empty -> 0
 
-(* Dla [a] > 0 zwraca a, wpp zwraca max_int *)
+(* Dla [a] >= 0 zwraca a, wpp zwraca max_int *)
 let safe_int a =
   if a < 0 then max_int else a
 
@@ -63,16 +65,17 @@ let safe_plus1 a =
 let safe_minus1 a =
   if a - 1 < a then a - 1 else a
   
-  
 (* Tworzy drzewo z korzeniem [k] i poddrzewami [l] oraz [r]
-   Zakładamy, że różnica wysokości tych poddrzew jest <=2 *)
+ * Zakładamy, że różnica wysokości tych poddrzew jest <=2 *)
 let make l k r =
-  Node (l, k, r, max (height l) (height r) + 1,
-  safe_int (safe_int (ile_int l + ile_int r) +
-  safe_plus1 (safe_int (snd k - fst k))))
+  let width = safe_int (safe_int (count_ints l + count_ints r) +
+  safe_plus1 (safe_int (snd k - fst k))) in
+  Node (l, k, r, max (height l) (height r) + 1, width)
 
-(* Balansuje drzewo, tzn za pomocą rotacji częściowo wyrównuje wysokości poddrzew
-   Różnica wysokości [l] i [r] musi być mniejsza równa 3, zwraca poprawne drzewo *)
+(* Balansuje drzewo, tzn częściowo wyrównuje wysokości poddrzew.
+ * Różnica wysokości [l] i [r] musi być mniejsza równa 3,
+ * ponadto l < k oraz k < r.
+ * Zwraca zbalansowane drzewo zawierające l, k, r.*)
 let bal l k r =
   let hl = height l in
   let hr = height r in
@@ -143,10 +146,9 @@ let rec add_one x = function
   | Empty -> make Empty x Empty
 
 (* Zwraca drzewo z dodatkowym wierzchołkiem - przedziałem:
-   dla przedziału (x, y) jeśli zmiejsz = -1, to dodaje przedział (x, y-1);
-   jeśli zmiejsz = 1, to przedział (x+1, y), dla zmniejsz = 0 przedział (x, y)
-   Dodatkowo sprzadza czy przedział jest poprawny, tzn czy x<=y.
-   *)
+ * dla przedziału (x, y) jeśli zmniejsz = -1, to dodaje przedział (x, y-1);
+ * jeśli zmniejsz = 1, to przedział (x+1, y), dla zmniejsz = 0 przedział (x, y)
+ * Dodatkowo sprawdza czy przedział jest poprawny, tzn czy x<=y. *)
 let rec add_pSet x zmniejsz set =
   if zmniejsz <> 0 && (fst x = snd x) then set else
   if zmniejsz =  1 then add_pSet (safe_plus1 (fst x),  snd x) 0 set else
@@ -154,12 +156,12 @@ let rec add_pSet x zmniejsz set =
   if snd x < fst x then set else
   add_one x set
   
-(* Zwraca złączenie dwóch drzew*)
+(* Zwraca złączenie dwóch drzew *)
 let rec join l v r =
   match (l, r) with
     (Empty, _) -> add_one v r
   | (_, Empty) -> add_one v l
-  | (Node(ll, lv, lr, lh, _), Node(rl, rv, rr, rh, _)) ->
+  | (Node (ll, lv, lr, lh, _), Node (rl, rv, rr, rh, _)) ->
       if lh > rh + 2 then bal ll lv (join lr v r) else
       if rh > lh + 2 then bal (join l v rl) rv rr else
       make l v r
@@ -177,8 +179,7 @@ let remove_pSet x set =
 (* Rozdziela zbiór na wierzchołki <= [x] oraz > [x] *)
 let split_pSet x set =
   let rec loop x = function
-      Empty ->
-        (Empty, Empty)
+    | Empty -> (Empty, Empty)
     | Node (l, v, r, _, _) ->
         let c = compare x v in
         if c < 0 then
@@ -188,10 +189,10 @@ let split_pSet x set =
   in
   loop x set
 
-(** [split x s] zwraca trójkę [(l, present, r)], gdzie
-    [l] to zbiór liczb mniejszych od [x];
-    [r] to zbiór liczb większych  od [x];
-    [present] to wartość logiczna sprawdzająca, czy [x] należy do [s]*)
+(* [split x s] zwraca trójkę [(l, present, r)], gdzie
+ * [l] to zbiór liczb mniejszych od [x];
+ * [r] to zbiór liczb większych  od [x];
+ * [present] to wartość logiczna sprawdzająca, czy [x] należy do [s]*)
 let split x set =
   let (left, right) = split_pSet (x, max_int) set in
   let present = if left = Empty then false else 
@@ -215,29 +216,30 @@ let find_lower x set =
   let (left, right) = split_pSet (x, max_int) set in
   if left = Empty then None else Some (max_elt left)
 
-(* Usuwa ze zbioru liczb wszystkie liczby w przedziale (x, y) *)
+(* Usuwa ze zbioru liczb wszystkie liczby w przedziale [x, y] *)
 let rec remove (x, y) set =
   match find_lower y set with
   | None -> set
   | Some (a, b) when b < x -> set
-  | Some (a, b) -> remove (x, y) (add_pSet (a, x) (-1)
-                   (add_pSet (y, b) 1 (remove_pSet (a, b) set)))
+  | Some (a, b) ->
+    remove (x, y) (add_pSet (a, x) (-1)
+    (add_pSet (y, b) 1 (remove_pSet (a, b) set)))
 
-(* Dodaje do zbioru liczb wszystkie liczby w przedziale (x, y) *)
+(* Dodaje do zbioru liczb wszystkie liczby w przedziale [x, y] *)
 let rec add (x, y) set =
   let set = remove (x, y) set in
   match find_lower (safe_plus1 y) set with
   | Some (a, b) when b = safe_minus1 x -> add (a, y) set
-  | Some (a, b) when a = safe_plus1  y -> add (x, b) set
+  | Some (a, b) when a = safe_plus1 y -> add (x, b) set
   | _ -> add_pSet (x, y) 0 set
 
 (* Zwraca wartość logiczną, czy dana liczba jest w zbiorze? *)
 let mem x set =
   match find_lower x set with
-  | Some (a, b) when a<=x && x<=b -> true
+  | Some (a, b) when a <= x && x <= b -> true
   | _ -> false
 
-(* Iteruje się f do wszystkich spójnych przedziałów w zbiorze *)
+(* Iteruje f po wszystkich spójnych przedziałach w w zbiorze *)
 let iter f set =
   let rec loop = function
     | Empty -> ()
@@ -245,9 +247,8 @@ let iter f set =
   loop set
 
 (* Odpowiednik fold_left na listach, liczy
-   [(f xN ... (f x2 (f x1 a))...)], gdzie x1
-   ... xN to posortowane spójne przedziały danego zbioru.
-*)
+ * [(f xN ... (f x2 (f x1 a))...)], gdzie x1
+ * ... xN to posortowane spójne przedziały danego zbioru. *)
 let fold f set acc =
   let rec loop acc = function
     | Empty -> acc
@@ -255,19 +256,19 @@ let fold f set acc =
           loop (f k (loop acc l)) r in
   loop acc set
 
-(* Zwraca posortowaną listę wzsystkich spójnych przedziałów *)
+(* Zwraca posortowaną listę wszystkich spójnych przedziałów *)
 let elements set = 
   let rec loop acc = function
-      Empty -> acc
-    | Node(l, k, r, _, _) -> loop (k :: loop acc r) l in
+    | Empty -> acc
+    | Node (l, k, r, _, _) -> loop (k :: loop acc r) l in
   loop [] set
   
-(** [below n s] zwraca ilość elementów [s] mniejsych lub równych [n].
-    Jeśli jest ich więcej niż max_int, zwraca max_int. *)
+(* [below n s] zwraca liczbę elementów [s] mniejszych lub równych [n].
+ * Jeśli jest ich więcej niż max_int, zwraca max_int. *)
 let below n s =
   match split n s with
-  | (l, false, _) -> ile_int l
-  | (l, true, _) -> safe_int (1 + ile_int l)
+  | (l, false, _) -> count_ints l
+  | (l, true, _) -> safe_plus1 (count_ints l)
   
 (* Testy *)
 (*
@@ -291,5 +292,4 @@ let a = add (-13, 8) a;;
 let a = add (-13, -2) a;;
 assert(mem 11 a = true);;
 assert(elements a = [(-20, -19); (-13, 18)]);;
-
 *)
