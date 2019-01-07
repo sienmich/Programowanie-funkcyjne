a3cef557a12bc81e25017565bfe23a24e498269b
Michal Siennicki <sienmich@gmail.com>
2019-01-07 23:43:30 +0100

    Usunięte błedy, dodane komentarze

--- Topol/topol.ml
+++ Topol/topol.ml
@@ -1,74 +1,57 @@
+(* Sortowanie topologiczne  *)
 (* Autor - Michał Siennicki *)
+(* Review - Piotr Jasiński  *)
 
-(** Sortowanie topologiczne *)
+open PMap
 
 (** wyjatek rzucany przez [topol] gdy zaleznosci sa cykliczne *)
 exception Cykliczne
-
 
 (** Dla danej listy [(a_1,[a_11;...;a_1n]); ...; (a_m,[a_m1;...;a_mk])] 
     zwraca liste, na ktorej kazdy z elementow a_i oraz a_ij wystepuje
     dokladnie raz i ktora jest uporzadkowana w taki sposob, ze kazdy
     element a_i jest przed kazdym z elementow a_i1 ... a_il *)
-(* ('a * 'a list) list -> 'a list *)
-
-let dodaj komu ile mapa zerowi =
-  let ile = ile + (try find komu mapa with Not_found -> 0) in
-  if ile = 0
-  then (add komu ile mapa, komu::zerowi)
-  else (add komu ile mapa, zerowi)
-
-let init lis =
-  let f mapa (ele, lista) =
-    List.fold_left (fun m e -> fst (dodaj e 1 m []) ) mapa lista in
-  List.fold_left f empty lis
-
-let mapa_kolejnosci lis =
-  let f mapa (ele, lista) =
-    add ele lista mapa in
-  List.fold_left f empty lis
-  
-let init2 lis =
-  let m = init lis in
-  let f zer (ele, lista) =
-    snd (dodaj ele 0 m zer) in
-  (m, List.fold_left f [] lis)
-
-(* zwraca (ile_wchodzi, kto_ma_0)  *)
-let usuwanko kolejnosc (ile_wchodzi, kto_ma_0) usuwany =
-   let lis = try find usuwany kolejnosc with Not_found -> [] in
-   let pom (ile_wchodzi, kto_ma_0) komu_zmiejsz =
-     dodaj komu_zmiejsz (-1) ile_wchodzi kto_ma_0 in
-   List.fold_left pom (ile_wchodzi, kto_ma_0) lis
-  
 let topol lis =
-  let kolejnosc = mapa_kolejnosci lis in
-  let (ile_wchodzi, kto_ma_0) = init2 lis in
+  (* Mapa, która konkretnemu a_i przyporządkowywuje listę elementów,
+     które muszą być po nim w kolejności topologicznej *)
+  let kolejnosc =
+    let f mapa (ele, lista) =
+      add ele ((try find ele mapa with Not_found -> []) @ lista) mapa in
+    List.fold_left f empty lis in
+  
+  (* Zwiększa lub zmiejsza licznik krawędzi wchodzących
+     do danego wierzchołka grafu *)
+  let dodaj komu ile_dodac mapa kto_ma_0 =
+    let ile = ile_dodac + (try find komu mapa with Not_found -> (-5)) in
+    if ile < (-2) then (add komu ile_dodac mapa, komu::kto_ma_0) else
+    if ile = 0 && ile_dodac = (-1)
+    then (add komu ile mapa, komu::kto_ma_0)
+    else (add komu ile mapa, kto_ma_0) in
+
+  (* Mapa służąca jako licznik krawędzi wchodzących do wierzchołka grafu
+     i lista wierzchołków, do których nie wchodzą nieprzetworzone krawędzie *)
+  let (ile_wchodzi, kto_ma_0) = 
+    let g mapa (ele, lista) =
+      List.fold_left (fun m e -> fst (dodaj e 1 m [])) mapa lista in
+    let m = List.fold_left g empty lis in
+    let f (m, zer) (ele, lista) =
+      dodaj ele 0 m zer in
+    List.fold_left f (m, []) lis in
+
+  (* Usuwa z grafu wierzchołek, do którego nie wchodzą żadne krawędzie.
+     Zwraca zaktualizuje (ile_wchodzi, kto_ma_0) *)
+  let usuwanko (ile_wchodzi, kto_ma_0) usuwany =
+    let lis = try find usuwany kolejnosc with Not_found -> [] in
+    let pom (ile_wchodzi, kto_ma_0) komu_zmiejsz =
+      dodaj komu_zmiejsz (-1) ile_wchodzi kto_ma_0 in
+    List.fold_left pom (ile_wchodzi, kto_ma_0) lis in
+  
+  (* Główny algorytm, który po kolei usuwa wierzchołki 
+     o zerowym stopniu wchodzącym. Zwraca kolejność topologiczną *)
   let rec f (ile_wchodzi, kto_ma_0)  = 
     match kto_ma_0 with
-    | h::t -> 
-      h :: (f (usuwanko kolejnosc (ile_wchodzi, t) h))
-
+    | h::t -> h::(f (usuwanko (ile_wchodzi, t) h))
     | _ when fold (+) ile_wchodzi 0 = 0 -> []
     | _ -> raise Cykliczne in
   f (ile_wchodzi, kto_ma_0)
-(*
-
-topol [(1, [2]); (2, [3]); (3, [2])];;
-
-*)
-(* val topol lis = *)
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
