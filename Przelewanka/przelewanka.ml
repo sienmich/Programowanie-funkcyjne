6123733828c2d36d6240129e27de0c6ac9c77d89
sienmich <sienmich@gmail.com>
2019-01-15 20:14:31 +0100

    Dodane komentarze

--- Przelewanka/przelewanka.ml
+++ Przelewanka/przelewanka.ml
@@ -1,88 +1,102 @@
 (* Przelewanka             *)
 (* Autor: Michał Siennicki *)
+(* Review: *)
 
+(* Rozwiązanie polega na sprawdzaniu wszystkich możliwych stanów,
+   gdzie stan to ilość wody w kolejnych pojemnikach.
+   Robię to w kolejności rosnącej liczby ruchów *)
+
+(* Hash tablica, w której pamiętam odwiedzone stany *)
 let sprawdzone_stany = Hashtbl.create 100000
-let kol = Queue.create ()
-let znalazl = ref (-1)
-let szukane = ref [||]
 
-let dodaj_na_kol woda ile_ruchow =
-  let woda = Array.to_list woda in
-  if false = Hashtbl.mem sprawdzone_stany woda then
+(* Kolejka, na którą wrzucam stany do sprawdzenia w kolejności rosnącej liczby ruchów *)
+let kol = Queue.create ()
+
+(* Jeśli algorytm znajdzie rozwiązanie, to jego wartość to wynik *)
+let wynik = ref (-1)
+
+(* Stan, który chcę osiągnąć, czyli podawany do funkcji przelewanka *)
+let szukany_stan = ref [||]
+
+(* Funkcja, która dodaje nowy stan na kolejkę do przeanalizowania,
+   jeśli wcześniej nie był on przetworzony *)
+let dodaj_na_kol stan ile_ruchow =
+  let stan = Array.to_list stan in
+  if not (Hashtbl.mem sprawdzone_stany stan) then
   begin
-    Hashtbl.add sprawdzone_stany woda 0;
-    let l = Array.of_list woda in
-    if l = !szukane then begin znalazl := ile_ruchow end;
+    Hashtbl.add sprawdzone_stany stan 0;
+    let l = Array.of_list stan in
+    if l = !szukany_stan then wynik := ile_ruchow;
     Queue.add (l, ile_ruchow) kol
   end
   
-let mozliwe_ruchy (woda, ile_ruchow) pojemnosci =
-  for i = 0 to Array.length woda - 1 do
-    for j = 0 to Array.length woda - 1 do
-      let ile_leje = min woda.(i) (pojemnosci.(j) - woda.(j)) in
-      if ile_leje > 0 then
-      begin
-        woda.(i) <- woda.(i) - ile_leje;
-        woda.(j) <- woda.(j) + ile_leje;
-        dodaj_na_kol woda (ile_ruchow + 1);
-        woda.(i) <- woda.(i) + ile_leje;
-        woda.(j) <- woda.(j) - ile_leje
-      end
-    done;
-    
-    let tmp = woda.(i) in
+(* Funkcja, która z danego stanu generuje kolejne stany
+   wykonując wszystkie możliwe ruchy *)
+let mozliwe_ruchy (stan, ile_ruchow) pojemnosci =
+  for i = 0 to Array.length stan - 1 do
+    let tmp = stan.(i) in
 
     if tmp <> 0 then
     begin
-      woda.(i) <- 0;
-      dodaj_na_kol woda (ile_ruchow + 1)
+      stan.(i) <- 0;
+      dodaj_na_kol stan (ile_ruchow + 1)
     end;
     
     if tmp <> pojemnosci.(i) then
     begin
-      woda.(i) <- pojemnosci.(i);
-      dodaj_na_kol woda (ile_ruchow + 1)
+      stan.(i) <- pojemnosci.(i);
+      dodaj_na_kol stan (ile_ruchow + 1)
     end;
     
-    woda.(i) <- tmp
+    stan.(i) <- tmp;
+    
+    for j = 0 to Array.length stan - 1 do
+      let ile_leje = min stan.(i) (pojemnosci.(j) - stan.(j)) in
+      if ile_leje > 0 then
+      begin
+        stan.(i) <- stan.(i) - ile_leje;
+        stan.(j) <- stan.(j) + ile_leje;
+        dodaj_na_kol stan (ile_ruchow + 1);
+        stan.(i) <- stan.(i) + ile_leje;
+        stan.(j) <- stan.(j) - ile_leje
+      end
+    done
   done
-
-let przelewanko tab =
+  
+(* Główny algorytm, który wykonuje kolejne ruchy
+   dopóki nie znajdzie rozwiązania *)
+let backtrack tab =
   Hashtbl.clear sprawdzone_stany;
   Queue.clear kol;
-  znalazl := -1;
-  szukane := Array.init (Array.length tab) (fun x -> snd tab.(x));
+  wynik := -1;
+  szukany_stan := Array.init (Array.length tab) (fun x -> snd tab.(x));
   let pojemnosci = Array.init (Array.length tab) (fun x -> fst tab.(x)) in
   dodaj_na_kol (Array.make (Array.length tab) 0) 0;
   let stan = ref (Queue.take kol) in
-  
   try
-  while !znalazl = -1 do
-    mozliwe_ruchy !stan pojemnosci;
-    stan := Queue.take kol
-  done;
-  !znalazl
+    while !wynik = -1 do
+      mozliwe_ruchy !stan pojemnosci;
+      stan := Queue.take kol
+    done;
+    !wynik
   with Queue.Empty -> -1
   
+(* Optymalizacja sprawdzająca czy wszystkie pojemniki są nie pełne i nie puste *)
 let optymalizacja_pelno_pusta tab =
-  let wyn = ref true in
-  let pom (a, b) = b = 0 || a = b in
-  for i = 0 to -1 + Array.length tab do
-    if pom tab.(i) then begin wyn := false end; 
-  done;
-  !wyn
+  let niepusty_niepelny (a, b) = b > 0 && a > b in
+  Array.fold_left (fun p i -> p && niepusty_niepelny i) true tab
   
+(* Optymalizacja sprawdzająca czy któraś szukana ilość wody
+   jest niepodzielna przez nwd pojemności pojemników *)
 let optymalizacja_nwd tab =
   let rec gcd a b =
     if b = 0 then a else gcd b (a mod b) in
-  let nwd = max 1 (Array.fold_left (fun x (a, _) -> gcd x a) 0 tab) in
-  Array.fold_left (fun x (_, b) -> x || (b mod nwd > 0)) false tab
+  let nwd = max 1 (Array.fold_left (fun wyn (a, _) -> gcd wyn a) 0 tab) in
+  Array.fold_left (fun wyn (_, b) -> wyn || (b mod nwd > 0)) false tab
 
+(* Funkcja, która po sprawdzeniu optymalizacji
+   uruchamia główny algorytm przeszukiwania stanów *)
 let przelewanka tab =
-  (* testy optymalizacji "choc jeden pelny/pusty" *)
   if optymalizacja_pelno_pusta tab && Array.length tab > 0 then -1 else
-
-  (* testy optymalizacji na -1, sprawdzanie nwd *)
   if optymalizacja_nwd tab then -1 else
-  
-  przelewanko tab
+  backtrack tab
